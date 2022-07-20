0f945cd00110e59a1473c3b6af111a1aaa9b6ce7
Annarchive <ytpt@mail.ru>
2022-07-21 02:17:23 +0400

    Cards may be disable for choosing; add styles in Card.module.css; Update state.js

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -7,12 +7,12 @@ const Cards = (props) => {
 
     return (
         <div className={c.cards_container}>
-            {cardsState.map(card => <Card key={card.id} flavour={card.flavour}
-                portions={card.portions} mouseCount={card.mouseCount} mouse={card.mouse}
-                kilos={card.kilos} footer={card.footer} link={card.link} dot={card.dot}
-                extra={card.extra} isAvailable={card.isAvailable}
-                footerNotChoosenCard={card.footerNotChoosenCard}
-                footerChoosenCard={card.footerChoosenCard}
+            {cardsState.map(card => <Card
+                key={card.id} flavour={card.flavour} portions={card.portions}
+                mouseCount={card.mouseCount} mouse={card.mouse} kilos={card.kilos}
+                link={card.link} dot={card.dot} extra={card.extra} isAvailable={card.isAvailable}
+                footerNotChosenCard={card.footerNotChosenCard}
+                footerChosenCard={card.footerChosenCard}
                 footerNotAvailableCard={card.footerNotAvailableCard} />
                 )
             }
